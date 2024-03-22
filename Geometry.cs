0af8961a372fa5d911d7355598e383721c8db632
Oleksandr Kapara <103050489+abdu1o@users.noreply.github.com>
2024-03-22 12:40:56 -0700

    Add files via upload

---
+++ Geometry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL1
+{
+    public static class Geometry
+    {
+        public static double SquareArea(double side)
+        {
+            return side * side;
+        }
+
+        public static double RectangleArea(double side1, double  side2)
+        {
+            return side1 * side2;
+        }
+
+        public static double TriangleArea(double side, double height)
+        {
+            return (side * height) / 2;
+        }
+    }
+
+}
---
+++ Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL1
+{
+    public static class Text
+    {
+        public static bool IsPalindrome(string text)
+        {
+            int left = 0;
+            int right = text.Length - 1;
+
+            while (left < right)
+            {
+                if (text[left] != text[right])
+                {
+                    return false;
+                }
+                    
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        public static int Count(string text)
+        {
+            return text.Length;
+        }
+
+    }
+}
