e22df75cf85efbdce89acd84cd6a4ff12d441cd8
akapa <akapa@DESKTOP-GA6HPG7>
2024-03-22 21:40:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_Implement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DLL1.Geometry.RectangleArea(2, 3));
+            Console.WriteLine(DLL1.Geometry.SquareArea(2));
+            Console.WriteLine(DLL1.Geometry.TriangleArea(2, 3));
+
+            Console.WriteLine(DLL1.Text.IsPalindrome("hooh"));
+            Console.WriteLine(DLL1.Text.Count("hooh"));
+        }
+    }
+}
